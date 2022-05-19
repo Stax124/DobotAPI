8c6e4d42d371c7dcdffd641a585c9f53b704dde3
JustABenda <benda.filip.fb@gmail.com>
2022-05-19 09:57:03 +0200

    Another Example

--- hella_example2.py
+++ hella_example2.py
@@ -11,15 +11,18 @@ bot.connect()
 
 sucktioncup = SuctionCup(bot)
 
-#ReleasePosition (x=283.0964660644531, y=19.580909729003906, z=-45.90699768066406, rotation=3.9566705226898193)
+#ReleasePosition1 (x=217.86207580566406, y=149.8310546875, z=16.965282440185547, rotation=34.51760482788086)
+#ReleasePosition2 (x=208.19082641601562, y=104.1470947265625, z=17.151718139648438, rotation=26.57642936706543)
 #MiddleSPosition (x=97.96730041503906, y=173.89041137695312, z=124.62773895263672, rotation=60.603729248046875)
 #GrabPosition (x=27.88756561279297, y=188.13694763183594, z=15.004730224609375, rotation=81.56843566894531)
-posGrab = Position(x=27.88756561279297, y=188.13694763183594,
-                   z=15.004730224609375, rotation=81.56843566894531)
-posMiddle = Position(x=97.96730041503906, y=173.89041137695312,
-                     z=124.62773895263672, rotation=60.603729248046875)
-posRelease = Position(x=283.0964660644531, y=19.580909729003906,
-                      z=-45.90699768066406, rotation=3.9566705226898193)
+posGrab = Position(x=251.52867126464844, y=-4.765195846557617,
+                   z=-42.128211975097656, rotation=-1.0853352546691895)
+posMiddle = Position(x=201.03648376464844, y=123.8348159790039,
+                     z=116.35449981689453, rotation=31.63231086730957)
+posRelease1 = Position(x=209.86575317382812, y=148.5121612548828,
+                       z=17.670623779296875, rotation=35.28525161743164)
+posRelease2 = Position(x=202.63885498046875, y=101.3697280883789,
+                       z=17.314048767089844, rotation=26.57642936706543)
 
 
 def main():
@@ -27,11 +30,12 @@ def main():
     bot.ir_toggle(True)
     lastGrab = time.time()
     max_delay = 20
-
+    count = 0
     while True:
         if(not bot.get_ir()):
             # print(time.time()-lastGrab)
-            bot.conveyor_belt(0.25, 1)
+            bot.conveyor_belt(0, 1)
+            pass
         else:
             while(bot.get_ir()):
                 pass
@@ -39,10 +43,20 @@ def main():
             bot.move_to_position(posGrab)
             sucktioncup.suck()
             bot.move_to_position(posMiddle)
-            bot.move_to_position(posRelease)
+            if(count == 0):
+                bot.move_to_position(posRelease1)
+                count += 1
+            elif(count == 1):
+                bot.move_to_position(posRelease2)
+                count += 1
             sucktioncup.blow()
             bot.move_to_position(posMiddle)
             sucktioncup.idle()
+            if(count == 2):
+                bot.conveyor_belt(0.25, -1)
+                count = 0
+                bot.delay()
+                bot.delay()
         time.sleep(0.1)
 
 
