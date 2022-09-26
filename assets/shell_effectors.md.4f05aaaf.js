8ece719513a6612a50f8d390c6b099fc2e07f4ae
Stax124 <Stax124@users.noreply.github.com>
2022-09-26 20:10:49 +0000

    deploy: 4591d6bb88f1175274f1765701fd9796429e309d

---
+++ assets/shell_effectors.md.4f05aaaf.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s,a}from"./app.24b9f6f2.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"shell/effectors.md","lastUpdated":1664223018000}'),o={name:"shell/effectors.md"},r=a("p",null,"WIP",-1),c=[r];function n(l,_,d,f,p,i){return s(),t("div",null,c)}const u=e(o,[["render",n]]);export{h as __pageData,u as default};
